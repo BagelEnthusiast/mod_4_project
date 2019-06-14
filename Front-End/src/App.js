6ec91db4cb626d413010807ba1e4c50fd2d4b5df
BagelEnthusiast <nathan.richardson94@gmail.com>
2019-06-14 15:52:30 -0500

    routes working

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIO from 'socket.io-client'
+
+const io = socketIO('http://10.185.0.210:8080')
+window.io = io
+
+io.on('message', payload => {
+  console.log(payload)
+})
+
+// var socket = io.connect('http://localhost:3003')
+
+//vidhi lecture code
+// import socketIO from 'socket.io-client'
+//const io =  socketIO('http://10.185.1.7:3003')
+
+//vidhi ip
+// const io = socketIO('http://10.185.0.210:8080')
+// window.io = io
+
+//io.emit('random', "table tennis*")  test in console
+
 
 function App() {
   return (
